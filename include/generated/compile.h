a5f743811f498ae02b5b97abf7214995702d93ab
sadven <mattitude2010@gmail.com>
2015-05-11 21:50:48 +1200

    changes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Sat May 9 16:20:49 NZST 2015"
+#define UTS_VERSION "#1 SMP PREEMPT Mon May 11 20:43:21 NZST 2015"
 #define LINUX_COMPILE_BY "matt"
 #define LINUX_COMPILE_HOST "matt-VirtualBox"
-#define LINUX_COMPILER "gcc version 4.7.4 (crosstool-NG linaro-1.13.1+bzr2694 - Linaro GCC 2014.06 - Cortex-A15) "
+#define LINUX_COMPILER "gcc version 4.9.3 20150501 (prerelease) (UBERTC-4.9.3) "
